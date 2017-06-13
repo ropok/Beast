8ee450df874a8b7d444ff8bbbb3e8abf80063a58
Vinnie Falco <vinnie.falco@gmail.com>
2017-06-13 14:11:33 -0700

    [WIP]

--- examples/server.hpp
+++ examples/server.hpp
@@ -22,10 +22,16 @@
 template<class Derived>
 class server
 {
+    class port_base
+    {
+        virtual ~port_base() = default;
+        virtual void close() = 0;
+    };
+
+    template<class AcceptHandler>
+    class port;
+
     boost::asio::io_service ios_;
-    boost::asio::ip::tcp::socket sock_;
-    boost::asio::ip::tcp::endpoint ep_;
-    boost::asio::ip::tcp::acceptor acceptor_;
     std::vector<std::thread> threads_;
     boost::optional<boost::asio::io_service::work> work_;
 
@@ -34,6 +40,8 @@ protected:
     using socket_type = boost::asio::ip::tcp::socket;
 
 public:
+    using endpoint_type = boost::asio::ip::tcp::endpoint;
+
     server(server const&) = delete;
     server& operator=(server const&) = delete;
 
@@ -68,12 +76,21 @@ public:
         @param ep The address and port to bind to.
 
         @param ec Set to the error, if any occurred.
+
+        @param handler The handler to associate with the listening
+        port, which will be copied or moved. The handler must have
+        this equivalent signature:
+        @code
+        void handler(
+            socket_type&& sock,     // The accepted socket
+            endpoint_type ep        // The remote endpoint
+        );
+        @endcode
     */
-    // VFALCO This can take the handler for new connections
-    //        instead of calling in to Derived, this way each
-    //        port has its own handler.
+    template<class AcceptHandler>
     void
-    open(boost::asio::ip::tcp::endpoint const& ep, error_code& ec);
+    open(boost::asio::ip::tcp::endpoint const& ep,
+        error_code& ec, AcceptHandler&& handler);
 
     /** Stop the server.
     */
@@ -92,6 +109,70 @@ private:
 
     void
     on_accept(error_code ec);
+};
+
+//------------------------------------------------------------------------------
+
+template<class AcceptHandler>
+class server::port : public port_base
+{
+    server& s_;
+    AcceptHandler hander_;
+    boost::asio::ip::tcp::socket sock_;
+    boost::asio::ip::tcp::endpoint ep_;
+    boost::asio::ip::tcp::acceptor acceptor_;
+
+public:
+    template<class DeducedHandler>
+    port(server& s, DeducedHandler&& handler)
+        : s_(s)
+        , handler_(std::forward<DeducedHandler>(handler))
+        , sock_(s_.get_io_service())
+        , acceptor(s_.get_io_service())
+    {
+    }
+
+    void
+    open(boost::asio::ip::tcp::endpoint const& ep)
+    {
+        acceptor_.open(ep.protocol(), ec);
+        if(ec)
+            return;
+        acceptor_.set_option(
+            boost::asio::socket_base::reuse_address{true});
+        acceptor_.bind(ep, ec);
+        if(ec)
+            return;
+        acceptor_.listen(
+            boost::asio::socket_base::max_connections, ec);
+        if(ec)
+            return;
+        acceptor_.async_accept(sock_, ep_,
+            std::bind(&server::on_accept, this,
+                std::placeholders::_1));
+    }
+
+    void
+    close() override
+    {
+        error_code ec;
+        acceptor_.close(ec);
+    }
+
+private:
+    void
+    on_accept(error_code ec)
+    {
+        if(! acceptor_.is_open())
+            return;
+        if(ec == boost::asio::error::operation_aborted)
+            return;
+        if(! ec)
+            handler_(std::move(sock_), ep_);
+        acceptor_.async_accept(sock_, ep_,
+            std::bind(&server::on_accept, this,
+                std::placeholders::_1));
+    }
 };
 
 //------------------------------------------------------------------------------
